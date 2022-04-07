663ea1b15661e9e5369a46dede369aa21a6bfb1f
FROSTYLAN <castillo089frosty@gmail.com>
2022-04-06 20:32:45 -0500

    redux,axios

---
+++ src/redux/index.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+    products: []
+}
+
+const reducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
