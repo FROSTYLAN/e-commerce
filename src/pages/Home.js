a990aa653f7bc9ad7b590e327c10754680336eb1
FROSTYLAN <castillo089frosty@gmail.com>
2022-04-11 03:03:40 -0500

    barra de búsqueda

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { filterCategoryThunk, getCategoryThunk, getProductsThunk } from '../redux/actions';
+import { filterCategoryThunk, filterProductNameThunk, getCategoryThunk, getProductsThunk } from '../redux/actions';
 import '../styles/home/styles.css';
 
 
@@ -9,6 +9,8 @@ const Home = () => {
     const [ isFocusFilter, setIsFocusFilter ] = useState(false);
     const [ dropDownPrice, setDropDownPrice] = useState(false);
     const [dropDownCategory, setDropDownCategory] = useState(false);
+
+    const [ productName, setProductName] = useState("");
 
     const dispatch = useDispatch();
     const products = useSelector(state => state.products);
@@ -19,30 +21,23 @@ const Home = () => {
         dispatch(getCategoryThunk());
     }, [dispatch])
 
+    const searchProducts = e => {
+        e.preventDefault();
+        console.log(productName);
+        dispatch(filterProductNameThunk(productName));
+    }
+
     return (
         <div className='home'>
 
-            <div className="search-box">
-                <button 
-                    className={isFocusFilter ? "filter-button focus" : "filter-button"}
-                    onClick={() => setIsFocusFilter(!isFocusFilter)}
-                >
-                    <i className="fa-solid fa-filter"></i>
-                     Filters
-                </button>
-                <div className={isFocusFilter ? "filters-modal open" : "filters-modal" }>
-                <button 
-                    className={isFocusFilter ? "close" : "close focus"}
-                    onClick={() => setIsFocusFilter(!isFocusFilter)}>
-                    x
-                </button>
-                    <h5>Filters</h5>
+            <aside>
+                <div className="fixed-filters">
                     <div className="filters">
                         <div
                             className={dropDownPrice ? "filter-drop-down closedContent" : "filter-drop-down"}
                         >
                             <div
-                                onClick={() => setDropDownPrice(!dropDownPrice)} 
+                                onClick={() => setDropDownPrice(!dropDownPrice)}
                                 className={dropDownPrice ? "header closed" : "header"}
                             >
                                 <span>Price</span>
@@ -62,7 +57,7 @@ const Home = () => {
                                 </form>
                             </div>
                         </div>
-                        <div 
+                        <div
                             className={dropDownCategory ? "filter-drop-down closedContent" : "filter-drop-down"}
                         >
                             <div
@@ -91,12 +86,96 @@ const Home = () => {
                                     }
                                 </ul>
                             </div>
-                        </div>      
+                        </div>
                     </div>
                 </div>
-            </div>
+            </aside>
 
-            <div className='main-container'>
+            <section className='main-container'>
+                <div className="search-box">
+                    <form onSubmit={searchProducts} class="input">
+                        <input 
+                            type="text" 
+                            placeholder='What are you looking for?'
+                            value={productName}
+                            onChange={e => setProductName(e.target.value)}
+                        />
+                        <button>
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </form>
+                    <button
+                        className={isFocusFilter ? "filter-button focus" : "filter-button"}
+                        onClick={() => setIsFocusFilter(!isFocusFilter)}
+                    >
+                        <i className="fa-solid fa-filter"></i>
+                        Filters
+                    </button>
+                    <div className={isFocusFilter ? "filters-modal open" : "filters-modal"}>
+                        <button
+                            className={isFocusFilter ? "close" : "close focus"}
+                            onClick={() => setIsFocusFilter(!isFocusFilter)}>
+                            x
+                        </button>
+                        <h5>Filters</h5>
+                        <div className="filters">
+                            <div
+                                className={dropDownPrice ? "filter-drop-down closedContent" : "filter-drop-down"}
+                            >
+                                <div
+                                    onClick={() => setDropDownPrice(!dropDownPrice)}
+                                    className={dropDownPrice ? "header closed" : "header"}
+                                >
+                                    <span>Price</span>
+                                    <i className="fa-solid fa-caret-down"></i>
+                                </div>
+                                <div className="content">
+                                    <form action="" className="price-filter">
+                                        <label htmlFor="from">
+                                            <span>From</span>
+                                            <input type="number" />
+                                        </label>
+                                        <label htmlFor="to">
+                                            <span>To</span>
+                                            <input type="number" />
+                                        </label>
+                                        <button>Filter price</button>
+                                    </form>
+                                </div>
+                            </div>
+                            <div
+                                className={dropDownCategory ? "filter-drop-down closedContent" : "filter-drop-down"}
+                            >
+                                <div
+                                    onClick={() => setDropDownCategory(!dropDownCategory)}
+                                    className={dropDownCategory ? "header closed" : "header"}
+                                >
+                                    <span>Category</span>
+                                    <i className="fa-solid fa-caret-down"></i>
+                                </div>
+                                <div className="content">
+                                    <ul className="category-filter">
+                                        {
+                                            categories.map(category => (
+                                                <li key={category.id}
+                                                    onClick={() => {
+                                                        dispatch(filterCategoryThunk(category.id));
+                                                        setIsFocusFilter(!isFocusFilter)
+                                                    }}
+                                                    className={isFocusFilter ? "close" : null}
+                                                >
+                                                    <span>
+                                                        {category.name}
+                                                    </span>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <ul className='products-list'>
                     {
                         products.map(product => (
@@ -124,7 +203,7 @@ const Home = () => {
                         ))
                     }
                 </ul>
-            </div>
+            </section>
         </div>
     );
 };
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -48,3 +48,11 @@ export const filterCategoryThunk = id => {
             .finally(() => dispatch(setIsLoading(false)));
     }
 }
+export const filterProductNameThunk = productName => {
+    return dispatch => {
+        dispatch(setIsLoading(true))
+        return axios.get(`https://ecommerce-api-react.herokuapp.com/api/v1/products?query=${productName}`)
+            .then(res => dispatch(setProducts(res.data.data.products)))
+            .finally(() => dispatch(setIsLoading(false)));
+    }
+}
